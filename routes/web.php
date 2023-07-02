f278546e2c55816584fc2571174e4eda4e9e22ba
hiu <95419527+Keipatr@users.noreply.github.com>
2023-07-02 22:00:22 +0800

    template
    isi template login dan contact

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+Route::get('contact', function () {
+    return view('contact');
 });
